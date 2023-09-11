b4e3539fb719ed38cefef80c786060f1432c6af2
abubluecorian <abu786.japit@gmail.com>
2023-09-11 20:44:44 +0530

    examples of switch statement

--- coursera.js
+++ coursera.js
@@ -1,3 +1,4 @@
+var light = green;
 if(light == "green") {
     console.log("Drive")
 } else if (light == "orange") {
@@ -27,3 +28,51 @@ switch(light) {
         console.log('The light is not green, orange, or red');
         break;
  }
+
+//  more examples of switch 
+// Code the days of the week program as a switch statement
+// On the next line, define a new variable, name it day, and set its value to "Sunday".
+
+// Start coding a switch statement, passing the day variable as the expression to evaluate.
+
+// Inside the switch, add cases for every day of the week, starting with 'Monday', and ending with 'Sunday'. Make sure to use string values for days. Inside each case, for now, just add a console.log('Do something'), and add a break; on the line below.
+
+// At the very bottom of the switch statement, add the default case and add a console.log('There is no such day').
+
+// Finally, update the console.log calls for each case, based on whatever activity you have on each of the days.
+
+// Tips
+// If you need to make sure that multiple conditions are true in an if statement, you can do so using the && operator
+
+// In JavaScript, the correct syntax of the "greater than or equal to" operator is: >=.
+
+// Don't forget to add a break at the very end of each case in a switch statement.
+var day = `Sunday`;
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       //this block will run if no condition matches
+       console.log('There is no such day');
+}
+
+// output - Have barbecue
