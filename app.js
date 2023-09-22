2f67b8d2d6c0cc5f36765a302e567f82068de7c3
abubluecorian <abu786.japit@gmail.com>
2023-09-22 18:36:07 +0530

    theme set to lightasDefault

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
+// (function () {
+//     [...document.querySelectorAll(".control")].forEach(button => {
+//         button.addEventListener("click", function() {
+//             document.querySelector(".active-btn").classList.remove("active-btn");
+//             this.classList.add("active-btn");
+//             document.querySelector(".active").classList.remove("active");
+//             document.getElementById(button.dataset.id).classList.add("active");
+//         })
+//     });
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("light-mode");
+//     })
+// })();
 (function () {
+    // Check if a theme preference is stored in local storage
+    const currentTheme = localStorage.getItem("theme");
+
+    // Set the default theme to "light" if no preference is found
+    if (!currentTheme) {
+        localStorage.setItem("theme", "light");
+    }
+
+    // Function to set the theme based on the current preference
+    function setTheme(theme) {
+        if (theme === "light") {
+            document.body.classList.add("light-mode");
+        } else {
+            document.body.classList.remove("light-mode");
+        }
+    }
+
+    // Initialize the theme based on the stored preference
+    setTheme(currentTheme);
+
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
@@ -7,7 +40,15 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
+
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+        // Toggle the theme when the theme button is clicked
+        if (document.body.classList.contains("light-mode")) {
+            setTheme("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            setTheme("light");
+            localStorage.setItem("theme", "light");
+        }
+    });
 })();
